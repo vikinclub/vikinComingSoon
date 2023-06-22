eb887e80231cc62bbc7ab83268defe4017096bd4
vikinclub <137322842+vikinclub@users.noreply.github.com>
2023-06-22 08:23:27 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Col, Row } from 'antd';
+
+function App() {
+    return (
+        <>
+            <div className='main-container'>
+                <div className='flex justify-center items-center h-screen w-screen'>
+                    <h1 className='text-6xl text-white tracking-wider'>vikin.club</h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
